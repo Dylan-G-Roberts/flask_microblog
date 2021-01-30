8f5e4a040b46f31957ceb36790a4615a04be0f59
Dylan Roberts <dylangardnerroberts@gmail.com>
2021-01-30 19:36:18 -0400

    WooHoo! Added in basic struct for html extending.

--- app/routes.py
+++ app/routes.py
@@ -5,4 +5,14 @@ from app import app
 @app.route('/index')
 def index():
     user = {'username': 'Dylan'}
-    return render_template('index.html', title='Home', user=user)
+    posts = [
+        {
+            'author': {'username': 'Dylan'},
+            'body': 'Beautiful day in Maine!'
+        },
+        {
+            'author': {'username': 'Jack'},
+            'body': 'I love watching Tenent!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
